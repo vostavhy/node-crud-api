a5f6d96b2928f862891ca109f26bcdb6891c54a5
vostavhy <vostavhy@gmail.com>
2022-06-19 22:43:40 +0300

    feat: implement users request

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 import http from 'http';
 import dotenv from 'dotenv';
+import users from './data/users.json' assert { type: 'json' };
 
 dotenv.config();
-console.log(process.env.PORT);
 
-const server = http.createServer((req, res) => {});
+const server = http.createServer((req, res) => {
+  if (req.url === '/api/users') {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(users));
+  } else {
+    res.writeHead(404, { 'Content-Type': 'application/json' });
+    res.end('Page not found');
+  }
+});
+
 const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
